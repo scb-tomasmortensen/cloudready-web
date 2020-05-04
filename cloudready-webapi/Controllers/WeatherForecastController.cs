c3fd03b61f4afbe145827918f9e17fc3464abc60
l4x3r <tomas@evosolution.se>
2020-05-04 08:29:13 +0200

    Added WebAPI with .Net Core 2.1 support

--- cloudready-webapi/Controllers/WeatherForecastController.cs
+++ cloudready-webapi/Controllers/WeatherForecastController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
-namespace cloudready_web.Controllers
+namespace cloudready_webapi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
