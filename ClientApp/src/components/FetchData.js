ed2d3729b010012e35d516f9cb7ba78e30f24a8d
l4x3r <tomas@evosolution.se>
2020-05-04 09:12:28 +0200

    Fetching data from hostname in OpenShift

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -52,7 +52,7 @@ export class FetchData extends Component {
   }
 
   async populateWeatherData() {
-    const response = await fetch('weatherforecast');
+    const response = await fetch('http://cloudready-webapi-cloudready.apps.ocp-eu2.prod.nextcle.com/weatherforecast');
     const data = await response.json();
     this.setState({ forecasts: data, loading: false });
   }
